b91b4c8f6d717ba06f525b369ab827ffdf3c3c48
nekagit <nenad99@hotmail.de>
2024-01-03 20:26:46 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
 import News from "./pages/News";
+import School from "./pages/School";
+import Sport from "./pages/Sport";
+import SportDays from "./pages/SportDays";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,26 +17,26 @@ function App() {
           path: "/index",
           element: <News />,
         },
-        // {
-        //   path: "/school",
-        //   element: <School />,
-        // },
-        // {
-        //   path: "/sport",
-        //   element: <Sport />,
-        // },
-        // {
-        //   path: "/gallery",
-        //   element: <Gallery />,
-        // },
-        // {
-        //   path: "/sport/sportDays",
-        //   element: <SportDays />,
-        // },
-        // {
-        //   path: "contact",
-        //   element: <Contact />,
-        // },
+        {
+          path: "/school",
+          element: <School />,
+        },
+        {
+          path: "/sport",
+          element: <Sport />,
+        },
+        {
+          path: "/gallery",
+          element: <Gallery />,
+        },
+        {
+          path: "/sport/sportDays",
+          element: <SportDays />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
       ],
     },
   ]);
--- src/components/Home/CardBox.tsx
+++ src/components/Home/CardBox.tsx
@@ -1,66 +1,38 @@
+import { useState } from "react";
 import "../../Cards.css";
 export default function CardBox() {
+  const [active, setActive] = useState(true);
+  const lines = [
+    { initial: "School", htmlFor: "c1", buttonName: "asdf" },
+    { initial: "Sport", htmlFor: "c2", buttonName: "asdf" },
+    { initial: "Sportski \nDani", htmlFor: "c3", buttonName: "asdf" },
+    { initial: "Pregled \nGodine", htmlFor: "c4", buttonName: "asdf" },
+  ];
   return (
-    <div className="wrapper">
-      <div className="container">
-        <input type="radio" name="slide" id="c1" />
-        <label htmlFor="c1" className="card">
-          <div className="column">
-            <div className="icon">1</div>
-            <div className="description">
-              <h4>Winter</h4>
-              <p className="hidden">
-                Winter has so much to offer - creative activities
-              </p>
-              <button className="button-30" role="button">
-                Button 30
-              </button>
-            </div>
-          </div>
-        </label>
-        <input type="radio" name="slide" id="c2" />
-        <label htmlFor="c2" className="card">
-          <div className="column">
-            <div className="icon">D</div>
-            <div className="description">
-              <h4>
-                <button className="button-30" role="button">
-                  Digital Technology
-                </button>
-              </h4>
-              <p className="hidden">Gets better every day - stay tuned</p>
-            </div>
-          </div>
-        </label>
-        <input type="radio" name="slide" id="c3" />
-        <label htmlFor="c3" className="card">
-          <div className="column">
-            <div className="icon">3</div>
-            <div className="description">
-              <h4>Globalization</h4>
-              <p className="hidden">Help people all over the world</p>
-              <button className="button-30" role="button">
-                Button 30
-              </button>
-            </div>
-          </div>
-        </label>
-        <input type="radio" name="slide" id="c4" />
-        <label htmlFor="c4" className="card">
-          <div className="column">
-            <div className="icon">4</div>
-            <div className="description">
-              <h4>New technologies</h4>
-              <p className="hidden">
-                Space engineering becomes more and more advanced
-              </p>
-              <button className="button-30" role="button">
-                Button 30
-              </button>
-            </div>
-          </div>
-        </label>
-      </div>
-    </div>
+      <section className="wrapper">
+        <div className="container">
+          {active ? (
+            <>
+              {lines.map((x) => (
+                <>
+                  <input type="radio" name="slide" id={x.htmlFor} />
+                  <label htmlFor={x.htmlFor} className="card">
+                    <div className="column">
+                      <div className="icon">{x.initial}</div>
+                      <div className="description">
+                        <h4>
+                          <button className="button-30">{x.buttonName}</button>
+                        </h4>
+                      </div>
+                    </div>
+                  </label>
+                </>
+              ))}
+            </>
+          ) : (
+            <>asdf</>
+          )}
+        </div>
+      </section>
   );
 }
